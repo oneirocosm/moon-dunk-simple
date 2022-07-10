bb0fa299d2f7646eedd70aac8c0c26812b8cd36e
oneirocosm <software@oneirocosm.com>
2022-07-10 13:34:51 -0700

    Fix await bug

--- index.js
+++ index.js
@@ -31,33 +31,31 @@ log(`Program starting at ${Date.now()}`);
 // set pin high to start (no water flows)
 solenoidCtrl.writeSync(1);
 
-while (true) {
-    socket.on("event", event => {
-        // This wouldn't be necessary if it weren't for the rogue 'streamlabels' event that is not an array
-        let unformatted = event.message instanceof Array ? event.message.pop() : event.message;
-    
-        // No message? Must be an error, so we skip it because we already do raw emits.
-        if(!(unformatted instanceof Object)) {
-            log(`Event ${event.event_id} had no ites in its event.message property, skipping.`);
-            return;
+socket.on("event", event => {
+    // This wouldn't be necessary if it weren't for the rogue 'streamlabels' event that is not an array
+    let unformatted = event.message instanceof Array ? event.message.pop() : event.message;
+
+    // No message? Must be an error, so we skip it because we already do raw emits.
+    if(!(unformatted instanceof Object)) {
+        log(`Event ${event.event_id} had no ites in its event.message property, skipping.`);
+        return;
+    }
+
+    log(`New streamlabs event: ${event.type} at ${Date.now()}`);
+
+    if (event.type == "donation") {
+        for (const instance of event.message) {
+            const amountUsd = Convert(instance.amount).from(instance.currency).to("USD");
+            const duration = amountUsd * TIME_PER_DOLLAR;
+            log(`${instance.from} donated ${amountUsd} USD for ${duration} seconds of water`)
+
+            // set pin low (water flows)
+            solenoidCtrl.writeSync(0);
+
+            sleep(duration)
+
+            // set pin high (water stops)
+            solenoidCtrl.writeSync(1);
         }
-
-        log(`New streamlabs event: ${event.type} at ${Date.now()}`);
-
-        if (event.type == "donation") {
-            for (const instance of event.message) {
-                const amountUsd = await Convert(instance.amount).from(instance.currency).to("USD");
-                const duration = amountUsd * TIME_PER_DOLLAR;
-                log(`${instance.from} donated ${amountUsd} USD for ${duration} seconds of water`)
-
-                // set pin low (water flows)
-                solenoidCtrl.writeSync(0);
-
-                sleep(duration)
-
-                // set pin high (water stops)
-                solenoidCtrl.writeSync(1);
-            }
-        }
-    });
-}
+    }
+});
